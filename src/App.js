6a013223f21a829ca0f59782f6a53d1be7de2067
v.miroshnychenko <v.miroshnychenko@zfort.com>
2020-02-13 19:11:19 +0200

    the one commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+//Action
+function uploadAvatarActionCreator(payload) {
+  console.log('dispatched action');
+
+  return {
+    type: 'UPLOAD_AVATAR',
+    payload,
+  };
+}
+//service
+function getBase64(url) {
+  return axios
+    .get(url, {
+      responseType: 'arraybuffer',
+    })
+    .then(response => Buffer.from(response.data, 'binary').toString('base64'));
+}
+
+//Dispatch
+function App({ src, uploadAvatarActionCreator }) {
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+  const showNavbar = async () => {
+    console.log('start receiving image base64');
+
+    const base64 = await getBase64('/logo192.png');
+    console.log('received');
+
+    const src = `data:image/png;base64,${base64}`;
+    uploadAvatarActionCreator(src);
+  };
+  useEffect(() => {
+    console.log('mount or update');
+    if (src) {
+      setIsNavbarVisible(isNavbarVisible => !isNavbarVisible);
+      console.log('showed navbar');
+    }
+  }, [src]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {isNavbarVisible && (
+        <div className='navbar'>
+          <span>I'm navabar</span>
+          <img src={src} alt='async avatar' />
+        </div>
+      )}
+      <button onClick={showNavbar}>Toggle Navbar</button>
     </div>
   );
 }
 
-export default App;
+export default connect(
+  state => ({ src: state.src }),
+  dispatch => ({
+    uploadAvatarActionCreator: data => {
+      dispatch(uploadAvatarActionCreator(data));
+    },
+  }),
+)(App);
